00b7875ca16d02e825827d8c35725d84c1e58c89
maleor <mathieu.dodard@gmail.com>
2018-11-26 16:45:39 +0100

    analyse des mots pour chercher les noms + debut noms composes

--- src/extractor/Parser.java
+++ src/extractor/Parser.java
@@ -1,12 +1,15 @@
 package extractor;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
+
+import org.apache.commons.collections4.trie.PatriciaTrie;
 
 import requeterRezo.Mot;
 import requeterRezo.RequeterRezoDump;
@@ -17,12 +20,15 @@ import template.Template_matrix;
 
 public class Parser {
 
-	public ArrayList<String> discovered_rel; // La liste des relations extraitent
-	public Template_matrix template_matrix; // La matrice de template
+	private ArrayList<String> discovered_rel; // La liste des relations extraites
+	
+	private Template_matrix template_matrix; // La matrice de template
 
-	public RequeterRezoDump systeme;
+	private RequeterRezoDump systeme;
 
-	public ArrayList<String> words;
+	private ArrayList<String> words;
+
+	private PatriciaTrie<Integer> mots_composes;
 
 	///////////////////
 	/** CONSTRUCTOR **/
@@ -34,6 +40,9 @@ public class Parser {
 		template_matrix = new Template_matrix("./data/templates");
 
 		words = new ArrayList<>();
+
+		mots_composes = new PatriciaTrie<>();
+		initLists();
 	}
 
 	//////////////////////
@@ -51,6 +60,8 @@ public class Parser {
 		int window_size, col, line, number_of_line, begin = 0;
 		int number_of_col = template_matrix.get_size();
 		String tmpString = "";
+		String previous = "";
+		String following = "";
 		FileWriter fw = new FileWriter("data/extracted.txt");
 
 		/* Pour chaque colonnes de la matrice */
@@ -63,29 +74,48 @@ public class Parser {
 			while (begin <= words.size() - window_size) {
 
 				tmpString = " ";
+				previous = " ";
+				following = " ";
 
-				for (int index = begin; index < begin + window_size; index++) {
-					tmpString = tmpString + words.get(index)
-							+ " "; /* on créé un string temporaire contenant la fenetre à analyser */
-				}
-				begin++;
+				/* On crée un string temporaire contenant la fenetre à analyser */
+				for (int index = begin; index < begin + window_size; index++)
+					tmpString = tmpString + words.get(index) + " ";
 
 				/*
 				 * On vérifie s'il y a un match entre la fenetre de string et un des templates
 				 */
-
-				analyseStringForName(tmpString);
-
 				for (line = 0; line < number_of_line; line++) {
 					if (t_match(template_matrix.get_template(col, line), tmpString)) {
-						fw.write(template_matrix.get_template(col, line).get_template_under_string_shape() + "\n");
+
+						/*********************************/
+
+						/* On recupere les 10 mots qui precedent le string temporaire */
+						for (int jndex = 10; jndex > 0; jndex--)
+							if (begin - jndex >= 0)
+								previous = previous + words.get(begin - jndex) + " ";
+						previous = analyseStringForName(previous);
+
+						/*********************************/
+
+						/* On recupere les 10 mots qui suivent le string temporaire */
+						int start = begin + window_size - 1;
+						for (int kndex = 1; kndex <= 10; kndex++)
+							if (start + kndex < words.size())
+								following = following + words.get(start + kndex) + " ";
+						// following = analyseStringForName(following);
+
+						/*********************************/
+
+						/* On ecrit en sortie le template avec ce qui precede et ce qui suit */
+						discovered_rel.add(previous + " --- " + template_matrix.get_template(col, line).get_relation() + " --- "
+								+ following);
+
 						break;
 					}
 				}
-
+				begin++;
 			}
 			begin = 0;
-
 		}
 		fw.close();
 
@@ -95,36 +125,72 @@ public class Parser {
 	/** PRIVATE METHODS **/
 	///////////////////////
 
+	/**
+	 * Vérifie si un chaine de caractère existe comme template
+	 * 
+	 * @param t
+	 *            Le template avec lequel on compare la chaine de caractères
+	 * 
+	 * @param word
+	 *            La chaine de caractère que l'on compare
+	 * 
+	 * @return True si la chaine de caractères est un template, false sinon.
+	 */
 	private boolean t_match(Template t, String word) {
 		return t.get_eln().equals(word);
 	}
 
-	private String analyseWord(String word) {
+	/**
+	 * Crée un Mot à partir d'un string et vérifie si ca peut etre un nom
+	 * 
+	 * @param word
+	 *            Le string à partir duquel on crée un Mot
+	 * 
+	 * @return True si word peut etre un nom, false sinon
+	 */
+	private boolean analyseWord(String word) {
 		if (word.equals(""))
-			return "";
+			return false;
 
-		System.out.println("Le mot est : " + word);
+		boolean toRet;
+
 		Mot m = systeme.requete(word, 4, Filtre.FiltreRelationsEntrantes);
-		if(m != null) 
-			if (estNom(m))
-				return "Un nom";
-			else
-				return "Pas un nom";
-		else
-			return "Non analysable";
-		// return getClaGrammFromWord(m, 0);
+
+		toRet = (m != null) ? estNom(m) : false;
+
+		return toRet;
+
 	}
 
-	private void analyseStringForName(String str) {
+	/**
+	 * Analyse un chaine de caractères pour vérifier si elle peut être utilisée
+	 * comme un nom ou un nom composé
+	 * 
+	 * @param str
+	 *            La chaine de caractères que l'on analyse
+	 * 
+	 * @return La chaine de caractères finale qui accompagnera la template
+	 */
+	private String analyseStringForName(String str) {
+		String finalName = "";
 		String[] wd = str.split(" ");
 
-		String cg;
 		for (String s : wd) {
-			cg = analyseWord(s);
-			System.out.println(cg);
+			if (analyseWord(s)) {
+				finalName = s;
+				break;
+			}
 		}
+
+		return finalName;
 	}
 
+	/**
+	 * Initialise la liste des mots qui sera utilisée pour créer des fenêtres de
+	 * String
+	 * 
+	 * @throws FileNotFoundException
+	 */
 	private void initWords() throws FileNotFoundException {
 		File data_file = new File("./data/wiki_sample.txt");
 
@@ -132,8 +198,9 @@ public class Parser {
 
 		while (scanner.hasNextLine()) {
 
-			String line = scanner.nextLine(); // lecture de la ligne
-			String[] tmpWords = line.split(" "); // recuperation des mots de la ligne
+			String line = scanner.nextLine();
+			String[] tmpWords = line.split(" ");
+
 			for (String string : tmpWords)
 				words.add(string);
 		}
@@ -142,35 +209,48 @@ public class Parser {
 	}
 
 	/**
-	 * Recuperer les classes grammaticales d'un mot
+	 * Analyse un Mot est vérifie s'il peut etre un nom
 	 * 
-	 * @param m
-	 *            Le mot dont on recupere les classes grammaticales
-	 * @param index
-	 *            Rang dans la liste des cla. gram. pour savoir laquelle on retoune.
+	 * @param cible
+	 *            Le Mot que l'on analyse
 	 * 
-	 * @return La classe grammaticale du mot m au rang index du mot / null sinon
+	 * @return True si le Mot peut etre un nom, false sinon.
 	 */
-	private String getClaGrammFromWord(Mot m, int index) {
+	private boolean estNom(Mot cible) {
 
-		HashMap<Integer, ArrayList<Voisin>> req = m.getRelations_sortantes();
-		ArrayList<Voisin> termes = req.get("r_pos");
+		ArrayList<Voisin> voisins = new ArrayList<>();
+		voisins = cible.getRelations_sortantes(4);
 
-		if (termes != null)
-			return termes.get(index).getNom();
-		else
-			return null;
-	}
-
-	static public boolean estNom(Mot cible) {
-
-		for (Voisin v : cible.getRelations_sortantes(4)) {
-			if (v.getNom().startsWith("Nom")) {
-				return true;
+		if (voisins != null) {
+			for (Voisin v : voisins) {
+				if (v.getNom().startsWith("Nom")) {
+					return true;
+				}
 			}
 		}
 
 		return false;
 	}
+	
+	private boolean estNomCompose(String str) {
+		boolean toRet = false;
+		
+		
+		
+		return toRet;
+	}
 
+	private void initLists() {
+
+		String line1;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("data/mots_composes.txt"));
+			while ((line1 = br.readLine()) != null) {
+				mots_composes.put(line1, 0);
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
